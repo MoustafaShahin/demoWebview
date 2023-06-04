db622fa1fb765ada53f4e3a3d78f9aaae43cced0
Moustafa Shahin <m0setafa2016@hotmail.com>
2023-06-04 11:52:30 +0300

    edit file types

--- app/src/main/java/com/like/likecard/webViewProject/MainActivity.kt
+++ app/src/main/java/com/like/likecard/webViewProject/MainActivity.kt
@@ -48,7 +48,9 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
-
+  window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }*/
 
         binding.web.settings.javaScriptEnabled = true
